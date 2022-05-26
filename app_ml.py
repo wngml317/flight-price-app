a007efed10c6999799b110b5faf14023a5b48019
wngml317 <wngml317@naver.com>
2022-05-26 12:26:55 +0900

    add

--- app_ml.py
+++ app_ml.py
@@ -1,16 +1,64 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 import joblib
+import datetime 
+from datetime import date
 
 def run_ml() :
     st.subheader('항공권 가격 예측 페이지입니다.')
 
+    df = pd.read_csv('data/Flight_Dataset.csv')
+
     regressor = joblib.load('data/regressor.pkl')
     scaler_X = joblib.load('data/scaler_X.pkl')
     scaler_y = joblib.load('data/scaler_y.pkl')
+    ct = joblib.load('data/ct.pkl')
 
-    airline_list = ['SpiceJet', 'AirAsia', 'Vistara', 'GO_FIRST', 'Indigo', 'Air_India']
+    airline_list = list(df['airline'].unique())
     airline = st.selectbox('항공선을 선택해주세요', airline_list)
 
-    stops = st.number_input('출발지와 목적지 도시 간의 경유지 수', min_value=0)
+    stops = st.number_input('출발지와 목적지 도시 간의 경유지 수', min_value=0, max_value=5)
 
-    seat_class = st.radio('좌석 클래스를 선택해주세요.', ['Economy', 'Bussiness'])
+    seat_choice = st.radio('좌석 클래스를 선택해주세요.', ['Economy', 'Business'])
+    if seat_choice == 'Economy' :
+        seat = 0 
+    elif seat_choice == 'Business' :
+        seat = 1
+
+    dep_list = list(df['source_city'].unique())
+    arr_list = list(df['destination_city'].unique())
+
+    day = st.date_input("날짜를 선택해주세요.", min_value = date.today())
+    today = date.today()
+    days_left = (day-today).days
+
+    st.subheader('출발 정보')
+    dep_city = st.selectbox('출발 도시를 선택하세요.', dep_list)
+    st.time_input("출발 시간을 입력하세요.", datetime.time())
+
+    st.subheader('도착 정보')
+    arr_city = st.selectbox('도착 도시를 선택하세요.', arr_list)
+    st.time_input("도착 시간을 입력하세요.", datetime.time())
+
+    # 지속시간 : 출발 도시와 도착 도시, 경유 수를  구한 후, 지속시간의  평균을 구한다. 
+    duration = df.loc[(df['source_city'] == dep_city) & (df['destination_city'] == arr_city) & (df['stops'] == stops), 'duration']
+    duration = round(duration.mean(), 2)
+    
+    if st.button('예측 시작하기') :
+        new_data = np.array([airline, stops, seat, duration, days_left])
+        
+        print(new_data)
+        new_data = new_data.reshape(1, 5)
+        new_data = ct.transform(new_data)
+
+        new_data = scaler_X.transform(new_data)
+
+        y_pred = regressor.predict(new_data)
+
+        y_pred = scaler_y.inverse_transform(y_pred)
+        y_pred = round(y_pred[0,0])
+
+        st.write("예측한 항공권 가격은 " + str(y_pred) + "원 입니다.")
+
+    
